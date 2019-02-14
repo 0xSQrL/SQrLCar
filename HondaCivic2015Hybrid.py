123243a16819c26f7d0acc0586f9c81378f57403
William Silloway <wsilloway@gmail.com>
2019-02-13 22:43:38 -0500

    Some live test edits

--- HondaCivic2015Hybrid.py
+++ HondaCivic2015Hybrid.py
@@ -34,32 +34,32 @@ class HondaCivic:
     def get_speed_mph(self):
         if HondaCivic.DEBUG:
             return 10.5
-        return math.floor(self.connection.query(obd.commands.SPEED).value.to("mph").magnitude * 5) / 5
+        return math.floor(self.try_query(obd.commands.SPEED).value.to("mph").magnitude * 5) / 5
 
     def get_odometer(self):
         if HondaCivic.DEBUG:
             return 12
-        return self.connection.query(HondaCivic.odomCMD, force=True).value.magnitude
+        return self.try_query(HondaCivic.odomCMD, force=True).value.magnitude
 
     def get_running_time(self):
         if HondaCivic.DEBUG:
             return 400
-        return self.connection.query(obd.commands.RUN_TIME).value.to("mph").magnitude
+        return self.try_query(obd.commands.RUN_TIME).value.to("mph").magnitude
 
     def get_battery(self):
         if HondaCivic.DEBUG:
             return 0.69
-        return self.connection.query(obd.commands.HYBRID_BATTERY_REMAINING).value.magnitude
+        return self.try_query(obd.commands.HYBRID_BATTERY_REMAINING).value.magnitude
 
     def get_pressure(self):
         if HondaCivic.DEBUG:
             return 98
-        return self.connection.query(obd.commands.BAROMETRIC_PRESSURE).value.magnitude
+        return self.try_query(obd.commands.BAROMETRIC_PRESSURE).value.magnitude
 
     def get_gas_percent(self):
         if HondaCivic.DEBUG:
             return 0.72
-        return self.connection.query(obd.commands.FUEL_LEVEL).value.magnitude
+        return self.try_query(obd.commands.FUEL_LEVEL).value.magnitude
 
     def get_gas_gallons(self):
         if HondaCivic.DEBUG:
@@ -70,4 +70,13 @@ class HondaCivic:
         if HondaCivic.DEBUG:
             return 400.7
         return self.get_gas_gallons() * HondaCivic.MILES_PER_GALLON
-
+        
+    def try_query(self, command, force=True):
+        try:
+            ret = self.connection.query(command, force=force)
+            if ret.value is not None:
+                return ret
+        except:
+            print('err')
+        self.retry_connection()
+        return -1 * obd.Unit.miles
